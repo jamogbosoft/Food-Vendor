590b9de43856699f4c27922a235aabb9ad41c508
Ogbonna, James Chinyere <jamogbosoft@gmail.com>
2023-08-23 19:03:46 +0100

    Update IFoodService.cs

--- BlazorEcom/Client/Services/FoodService/IFoodService.cs
+++ BlazorEcom/Client/Services/FoodService/IFoodService.cs
@@ -5,11 +5,7 @@ namespace BlazorEcom.Client.Services.FoodService
     public interface IFoodService
     {
         List<Food> Foods { get; set; }
-
-        [HttpGet]
         Task GetFoods();
-
-        [HttpGet("{foodId}")]
         Task<ServiceResponse<Food>> GetFood(int foodId);
     }
 }
